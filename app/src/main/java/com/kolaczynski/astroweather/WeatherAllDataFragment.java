fe1e63563ee465b89b3ac9ac40fb55e96f29dd95
Jakub Kolaczynski <kubak95@gmail.com>
2021-01-14 23:28:21 +0100

    Tablet weather

--- app/src/main/java/com/kolaczynski/astroweather/WeatherAllDataFragment.java
+++ app/src/main/java/com/kolaczynski/astroweather/WeatherAllDataFragment.java
@@ -1,9 +1,16 @@
 package com.kolaczynski.astroweather;
 
 import android.os.Bundle;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
+import android.view.View.OnClickListener;
 import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.ImageView;
+import android.widget.TextView;
+import android.widget.Toast;
 
 import androidx.fragment.app.Fragment;
 
@@ -12,13 +19,13 @@ import androidx.fragment.app.Fragment;
  * Use the {@link WeatherAllDataFragment#newInstance} factory method to
  * create an instance of this fragment.
  */
-public class WeatherAllDataFragment extends Fragment {
+public class WeatherAllDataFragment extends Fragment implements OnClickListener {
 
     // TODO: Rename parameter arguments, choose names that match
     // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
     private static final String ARG_PARAM1 = "param1";
     private static final String ARG_PARAM2 = "param2";
-
+    Button acceptWeatherButton;
     // TODO: Rename and change types of parameters
     private String mParam1;
     private String mParam2;
@@ -58,6 +65,61 @@ public class WeatherAllDataFragment extends Fragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_weather_all_data, container, false);
+        View view = inflater.inflate(R.layout.fragment_weather_all_data, container, false);
+        acceptWeatherButton = view.findViewById(R.id.button_weather_accept);
+        acceptWeatherButton.setOnClickListener(this);
+        return view;
+    }
+
+    @Override
+    public void onClick(View v) {
+        if (v == acceptWeatherButton) {
+            EditText locationinput = getView().findViewById(R.id.location_input);
+            Log.d("Location", locationinput.getText().toString());
+            String myUrl = OpenWeatherAPI.currentWeatherRequestString + locationinput.getText().toString().replace(" ", "%20");
+            Log.d("Location", myUrl);
+            HttpGetRequest getRequest = new HttpGetRequest();
+            try {
+                String result = getRequest.execute(myUrl).get();
+                JSONParser.parseJSON(result);
+
+            } catch (Exception e) {
+                e.printStackTrace();
+                JSONParser.parseFailed();
+                Toast.makeText(getContext(), "Nie udało się pobrać danych", Toast.LENGTH_SHORT).show();
+            }
+
+            TextView longitude_weather_value, latitude_weather_value, LocationTime, temperature_value, pressure_value, description_view, wind_speed_value, wind_direction_value, humidity_value, visibility_value;
+            ImageView imageView;
+            int imagePath = getResources().getIdentifier("i" + OpenWeatherAPI.icon, "drawable", getContext().getPackageName());
+            imageView = getView().findViewById(R.id.imageView);
+            imageView.setImageResource(imagePath);
+
+
+            longitude_weather_value = getView().findViewById(R.id.longitude_weather_value);
+            latitude_weather_value = getView().findViewById(R.id.latitude_weather_value);
+            LocationTime = getView().findViewById(R.id.LocationTime);
+            temperature_value = getView().findViewById(R.id.temperature_value);
+            pressure_value = getView().findViewById(R.id.pressure_value);
+            description_view = getView().findViewById(R.id.description_view);
+
+            wind_speed_value = getView().findViewById(R.id.wind_speed_value);
+            wind_direction_value = getView().findViewById(R.id.wind_direction_value);
+            humidity_value = getView().findViewById(R.id.humidity_value);
+            visibility_value = getView().findViewById(R.id.visibility_value);
+
+
+            longitude_weather_value.setText(String.valueOf(OpenWeatherAPI.coordLon));
+            latitude_weather_value.setText(String.valueOf(OpenWeatherAPI.coordLat));
+            LocationTime.setText(OpenWeatherAPI.time);
+            temperature_value.setText(String.valueOf(OpenWeatherAPI.temperature));
+            pressure_value.setText(String.valueOf(OpenWeatherAPI.pressure));
+            description_view.setText(OpenWeatherAPI.description);
+
+            wind_speed_value.setText(String.valueOf(OpenWeatherAPI.windSpeed));
+            wind_direction_value.setText(String.valueOf(OpenWeatherAPI.windDirection));
+            humidity_value.setText(String.valueOf(OpenWeatherAPI.humidity));
+            visibility_value.setText(String.valueOf(OpenWeatherAPI.visibility));
+        }
     }
 }
--- app/src/main/java/com/kolaczynski/astroweather/WeatherDataFragment.java
+++ app/src/main/java/com/kolaczynski/astroweather/WeatherDataFragment.java
@@ -4,8 +4,8 @@ import android.os.Bundle;
 import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
-import android.view.ViewGroup;
 import android.view.View.OnClickListener;
+import android.view.ViewGroup;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.ImageView;
@@ -19,7 +19,7 @@ import androidx.fragment.app.Fragment;
  * Use the {@link WeatherDataFragment#newInstance} factory method to
  * create an instance of this fragment.
  */
-public class WeatherDataFragment extends Fragment implements OnClickListener{
+public class WeatherDataFragment extends Fragment implements OnClickListener {
 
     // TODO: Rename parameter arguments, choose names that match
     // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
@@ -71,56 +71,55 @@ public class WeatherDataFragment extends Fragment implements OnClickListener{
         return view;
     }
 
-@Override
-public void onClick(View v) {
-    if (v == acceptWeatherButton) {
-        EditText locationinput = getView().findViewById(R.id.location_input);
-        Log.d("Location", locationinput.getText().toString());
-        String myUrl = OpenWeatherAPI.currentWeatherRequestString + locationinput.getText().toString().replace(" ", "%20");
-        Log.d("Location", myUrl);
-        HttpGetRequest getRequest = new HttpGetRequest();
-        try {
-            String result = getRequest.execute(myUrl).get();
-            JSONParser.parseJSON(result);
+    @Override
+    public void onClick(View v) {
+        if (v == acceptWeatherButton) {
+            EditText locationinput = getView().findViewById(R.id.location_input);
+            Log.d("Location", locationinput.getText().toString());
+            String myUrl = OpenWeatherAPI.currentWeatherRequestString + locationinput.getText().toString().replace(" ", "%20");
+            Log.d("Location", myUrl);
+            HttpGetRequest getRequest = new HttpGetRequest();
+            try {
+                String result = getRequest.execute(myUrl).get();
+                JSONParser.parseJSON(result);
 
-        } catch (Exception e) {
-            e.printStackTrace();
-            JSONParser.parseFailed();
-            Toast.makeText(getContext(), "Nie udało się pobrać danych", Toast.LENGTH_SHORT).show();
+            } catch (Exception e) {
+                e.printStackTrace();
+                JSONParser.parseFailed();
+                Toast.makeText(getContext(), "Nie udało się pobrać danych", Toast.LENGTH_SHORT).show();
+            }
+
+            TextView longitude_weather_value, latitude_weather_value, LocationTime, temperature_value, pressure_value, description_view, wind_speed_value, wind_direction_value, humidity_value, visibility_value;
+            ImageView imageView;
+            int imagePath = getResources().getIdentifier("i" + OpenWeatherAPI.icon, "drawable", getContext().getPackageName());
+            imageView = getView().findViewById(R.id.imageView);
+            imageView.setImageResource(imagePath);
+
+
+            longitude_weather_value = getView().findViewById(R.id.longitude_weather_value);
+            latitude_weather_value = getView().findViewById(R.id.latitude_weather_value);
+            LocationTime = getView().findViewById(R.id.LocationTime);
+            temperature_value = getView().findViewById(R.id.temperature_value);
+            pressure_value = getView().findViewById(R.id.pressure_value);
+            description_view = getView().findViewById(R.id.description_view);
+
+            wind_speed_value = getView().findViewById(R.id.wind_speed_value);
+            wind_direction_value = getView().findViewById(R.id.wind_direction_value);
+            humidity_value = getView().findViewById(R.id.humidity_value);
+            visibility_value = getView().findViewById(R.id.visibility_value);
+
+
+            longitude_weather_value.setText(String.valueOf(OpenWeatherAPI.coordLon));
+            latitude_weather_value.setText(String.valueOf(OpenWeatherAPI.coordLat));
+            LocationTime.setText(OpenWeatherAPI.time);
+            temperature_value.setText(String.valueOf(OpenWeatherAPI.temperature));
+            pressure_value.setText(String.valueOf(OpenWeatherAPI.pressure));
+            description_view.setText(OpenWeatherAPI.description);
+
+            wind_speed_value.setText(String.valueOf(OpenWeatherAPI.windSpeed));
+            wind_direction_value.setText(String.valueOf(OpenWeatherAPI.windDirection));
+            humidity_value.setText(String.valueOf(OpenWeatherAPI.humidity));
+            visibility_value.setText(String.valueOf(OpenWeatherAPI.visibility));
         }
-
-        TextView longitude_weather_value, latitude_weather_value, LocationTime, temperature_value, pressure_value, description_view, wind_speed_value, wind_direction_value, humidity_value, visibility_value;
-        ImageView imageView;
-        int imagePath = getResources().getIdentifier("i" + OpenWeatherAPI.icon, "drawable", getContext().getPackageName());
-        imageView = getView().findViewById(R.id.imageView);
-        imageView.setImageResource(imagePath);
-
-
-        longitude_weather_value = getView().findViewById(R.id.longitude_weather_value);
-        latitude_weather_value = getView().findViewById(R.id.latitude_weather_value);
-        LocationTime = getView().findViewById(R.id.LocationTime);
-        temperature_value = getView().findViewById(R.id.temperature_value);
-        pressure_value = getView().findViewById(R.id.pressure_value);
-        description_view = getView().findViewById(R.id.description_view);
-
-        wind_speed_value = getView().findViewById(R.id.wind_speed_value);
-        wind_direction_value = getView().findViewById(R.id.wind_direction_value);
-        humidity_value = getView().findViewById(R.id.humidity_value);
-        visibility_value = getView().findViewById(R.id.visibility_value);
-
-
-
-        longitude_weather_value.setText(String.valueOf(OpenWeatherAPI.coordLon));
-        latitude_weather_value.setText(String.valueOf(OpenWeatherAPI.coordLat));
-        LocationTime.setText(OpenWeatherAPI.time);
-        temperature_value.setText(String.valueOf(OpenWeatherAPI.temperature));
-        pressure_value.setText(String.valueOf(OpenWeatherAPI.pressure));
-        description_view.setText(OpenWeatherAPI.description);
-
-        wind_speed_value.setText(String.valueOf(OpenWeatherAPI.windSpeed));
-        wind_direction_value.setText(String.valueOf(OpenWeatherAPI.windDirection));
-        humidity_value.setText(String.valueOf(OpenWeatherAPI.humidity));
-        visibility_value.setText(String.valueOf(OpenWeatherAPI.visibility));
     }
-}
 }
